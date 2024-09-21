223486f495329908e2234c5c6d5dfd18d87360f4
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-20 19:41:51 -0500

    Refactorizar componentes de formulario y tabla

--- src/components/ButtonSupplierForm.jsx
+++ src/components/ButtonSupplierForm.jsx
@@ -1,7 +1,3 @@
-import { suppliersFormSchema } from "@/lib/zod-validations/suppliersFormSchema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useEffect, useState } from "react";
-import { useForm, FormProvider } from "react-hook-form";
 import {
   Dialog,
   DialogContent,
@@ -10,49 +6,17 @@ import {
   DialogTitle,
   DialogTrigger,
   Button,
-  FormItem,
-  FormLabel,
-  FormControl,
-  FormMessage,
 } from "@/components/ui";
 import { Plus } from "lucide-react";
 import { useSupplier } from "@/hooks/useSupplier";
+import { SupplierForm } from "./SupplierForm";
+import { FormProvider } from "react-hook-form";
 
 export const ButtonSupplierForm = ({ editData, setEditData }) => {
-  const { addSupplier, editSupplier } = useSupplier();
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
-  const methods = useForm({
-    resolver: zodResolver(suppliersFormSchema),
-  });
-
-  const { register, handleSubmit, reset, setValue, formState: { errors } } = methods;
-
-  useEffect(() => {
-    if (editData) {
-      initializeForm(editData);
-      setIsDialogOpen(true);
-    }
-  }, [editData, setValue]);
-
-  const initializeForm = (data) => {
-    setValue("name", data.name);
-    setValue("dni", data.dni);
-    setValue("email", data.email);
-    setValue("phone", data.phone);
-  };
-
-  const onSubmit = (data) => {
-    if (editData) {
-      editSupplier({ ...data, id: editData.id });
-      setEditData(null);
-    } else {
-      addSupplier({ ...data, id: Date.now() });
-    }
-    reset();
-    setIsDialogOpen(false);
-  };
-
+  const { isDialogOpen, setIsDialogOpen, methods, handleSubmit } = useSupplier(
+    editData,
+    setEditData
+  );
   return (
     <div className="flex justify-end mb-4">
       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
@@ -68,41 +32,8 @@ export const ButtonSupplierForm = ({ editData, setEditData }) => {
             </DialogTitle>
           </DialogHeader>
           <FormProvider {...methods}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="grid grid-cols-2 gap-10 py-4">
-                <div className="flex flex-col gap-4">
-                  <FormItem>
-                    <FormLabel htmlFor="name">Nombre</FormLabel>
-                    <FormControl>
-                      <input id="name" {...register("name")} className="input" />
-                    </FormControl>
-                    {errors.name && <FormMessage>{errors.name.message}</FormMessage>}
-                  </FormItem>
-                  <FormItem>
-                    <FormLabel htmlFor="dni">DNI</FormLabel>
-                    <FormControl>
-                      <input id="dni" {...register("dni")} className="input" />
-                    </FormControl>
-                    {errors.dni && <FormMessage>{errors.dni.message}</FormMessage>}
-                  </FormItem>
-                </div>
-                <div className="flex flex-col gap-4">
-                  <FormItem>
-                    <FormLabel htmlFor="email">Correo</FormLabel>
-                    <FormControl>
-                      <input id="email" {...register("email")} className="input" />
-                    </FormControl>
-                    {errors.email && <FormMessage>{errors.email.message}</FormMessage>}
-                  </FormItem>
-                  <FormItem>
-                    <FormLabel htmlFor="phone">Telefono</FormLabel>
-                    <FormControl>
-                      <input id="phone" {...register("phone")} className="input" />
-                    </FormControl>
-                    {errors.phone && <FormMessage>{errors.phone.message}</FormMessage>}
-                  </FormItem>
-                </div>
-              </div>
+            <form onSubmit={handleSubmit}>
+              <SupplierForm />
               <DialogFooter>
                 <Button type="submit">Guardar Cambios</Button>
               </DialogFooter>
--- src/hooks/useSupplier.js
+++ src/hooks/useSupplier.js
@@ -1,14 +1,50 @@
+import { suppliersFormSchema } from "@/lib/zod-validations/suppliersFormSchema";
 import { useSupplierStore } from "@/stores/useSupplierStore";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
-export const useSupplier = () => {
+export const useSupplier = (editData, setEditData) => {
   const suppliers = useSupplierStore((state) => state.supplier);
   const addSupplier = useSupplierStore((state) => state.addSupplier);
   const editSupplier = useSupplierStore((state) => state.editSupplier);
   const deleteSupplier = useSupplierStore((state) => state.deleteSupplier);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const methods = useForm({
+    resolver: zodResolver(suppliersFormSchema),
+  });
+  const { handleSubmit, reset, setValue } = methods;
+
+  useEffect(() => {
+    if (editData) {
+      setValue("name", editData.name);
+      setValue("dni", editData.dni);
+      setValue("email", editData.email);
+      setValue("phone", editData.phone);
+      setIsDialogOpen(true);
+    }
+  }, [editData, setValue]);
+
+  const onSubmit = (data) => {
+    if (editData) {
+      editSupplier({ ...data, id: editData.id });
+      setEditData(null);
+    } else {
+      addSupplier({ ...data, id: Date.now() });
+    }
+    reset();
+    setIsDialogOpen(false);
+  };
+  
   return {
     addSupplier,
     editSupplier,
     suppliers,
     deleteSupplier,
+    onSubmit,
+    isDialogOpen,
+    setIsDialogOpen,
+    methods,
+    handleSubmit: handleSubmit(onSubmit),
   };
 };
