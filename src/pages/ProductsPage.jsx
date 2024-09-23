4f755b7a5515e882a17e5a8a126e7427cc3bfbdf
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-09-22 23:32:03 -0500

    feat: añadir página de productos y componente CardProduct

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,22 @@
+import { CardProduct } from "@/components/CardProduct";
+
 export const ProductsPage = () => {
   return (
-    <div>ProductsPage</div>
-  )
-}
-
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+      <CardProduct />
+    </div>
+  );
+};
