0c766535244ac2c776028e5a2a88457da56700a8
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-10-01 23:16:54 -0500

    Refactorizar componente TableInformation y PersonalTable
    Se refactorizó el componente TableInformation y se renombró a PersonalTable para reflejar mejor su propósito y contenido. Además, se realizaron cambios en la estructura de las tablas, agregando estilos y centrando el contenido de las celdas.

--- src/components/Tables/PersonalTable.jsx
+++ src/components/Tables/PersonalTable.jsx
@@ -16,19 +16,19 @@ export const TableInformation = ({ personal, setEditData }) => {
     <div className="overflow-x-auto">
       <Table className="min-w-full divide-y divide-gray-200">
         <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Nombre</TableHead>
+          <TableRow className='text-center'>
+            <TableHead>Nombre</TableHead>
             <TableHead>DNI</TableHead>
             <TableHead>Correo</TableHead>
             <TableHead>Telefono</TableHead>
-            <TableHead className="text-center">Acciones</TableHead>
+            <TableHead >Acciones</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody className="bg-white divide-y divide-gray-200">
           {personal.map((person) => (
             <TableRow key={person.id}>
-              <TableCell className="font-medium">{person.name}</TableCell>
-              <TableCell className="text-left">{person.dni}</TableCell>
+              <TableCell >{person.name}</TableCell>
+              <TableCell >{person.dni}</TableCell>
               <TableCell>{person.email}</TableCell>
               <TableCell>{person.phone}</TableCell>
               <TableCell className="text-center">
