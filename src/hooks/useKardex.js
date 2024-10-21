0b4073d89fdd88db02ed9970f7047b6c2480ec96
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2024-10-20 19:44:12 -0500

    update kardex

--- src/hooks/useKardex.js
+++ src/hooks/useKardex.js
@@ -1,4 +1,5 @@
 import { kardexFormSchema } from "@/lib/zod-validations/kardexFormSchema";
+import { createKardex, fillAllKardex, removeKardex, updateKardex } from "@/services/kardex.service";
 import { useKardexStore } from "@/stores/useKardexStore";
 import { Toast } from "@/utils/Toast";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -15,9 +16,14 @@ export const useKardex = (editData, setEditData) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => {
+    const fetchKardex = async () => {
+      const data = await fillAllKardex();
+      if (data) {
+        useKardexStore.setState({ kardex: data });
+      }
       setIsLoading(false);
-    }, 2000);
+    }
+    fetchKardex();
   }, []);
 
   const methods = useForm({
@@ -28,7 +34,7 @@ export const useKardex = (editData, setEditData) => {
   useEffect(() => {
     if (editData) {
       setValue("id", editData.id);
-      setValue("product", editData.product);
+      setValue("product", editData.productName);
       setValue("tipoTransacction", editData.tipoTransacction);
       setValue("count", editData.count);
       setValue("description", editData.description);
@@ -40,29 +46,42 @@ export const useKardex = (editData, setEditData) => {
     }
   }, [editData, setValue]);
 
-  const onSubmit = (data) => {
-    console.log("onSubmit called with data:", data);
-    if (editData) {
-      editKardex({ ...data, id: editData.id });
-      setEditData(null);
+  const onSubmit = async (data) => {
+    const kardexData = {
+      ...data,
+      productName: data.productName, 
+      user: data.user,
+      supplier: data.supplier,
+    };
+  
+    try {
+      if (editData) {
+        const updatedKardex = await updateKardex(editData.id, kardexData);
+        editKardex(updatedKardex);
+        setEditData(null);
+        Toast.fire({
+          icon: "success",
+          title: "Kardex actualizado con éxito",
+        });
+      } else {
+        const newKardex = await createKardex(kardexData);
+        addKardex(newKardex);
+        Toast.fire({
+          icon: "success",
+          title: "Kardex creado con éxito",
+        });
+      }
+      reset();
+      setIsDialogOpen(false);
+    } catch (error) {
+      console.error("Error al crear o actualizar el kardex:", error);
       Toast.fire({
-        icon: "success",
-        title: "Kardex actualizado con éxito",
-      });
-    } else {
-      const newKardex = { ...data, id: Date.now() };
-      addKardex(newKardex);
-      console.log(newKardex);
-      Toast.fire({
-        icon: "success",
-        title: "Kardex creado con éxito",
+        icon: "error",
+        title: "Error al crear o actualizar el kardex",
       });
     }
-    reset();
-    setIsDialogOpen(false);
-
   };
-
+  
   const handleDelete = (id) => {
     Swal.fire({
       title: "¿Estás seguro?",
@@ -72,14 +91,24 @@ export const useKardex = (editData, setEditData) => {
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
       confirmButtonText: "Sí, ¡elimínalo!",
-    }).then((result) => {
+    }).then(async (result) => {
       if (result.isConfirmed) {
-        deleteKardex(id);
-        Swal.fire({
-          title: "¡Eliminado!",
-          text: "El registro ha sido eliminado.",
-          icon: "success",
-        });
+        try {
+          await deleteKardex(id); 
+          removeKardex(id); 
+          Swal.fire({
+            title: "¡Eliminado!",
+            text: "El kardex ha sido eliminado.",
+            icon: "success",
+          });
+        } catch (error) {
+          console.error("Error al eliminar la kardex:", error);
+          Swal.fire({
+            title: "Error",
+            text: "No se pudo eliminar la kardex.",
+            icon: "error",
+          });
+        }
       }
     });
   };
